8a74829633500b8f1072183c66fb67c46ee27f43
jhonata velandia <jhonatav@localhost.localdomain>
2024-10-25 21:02:31 -0500

    find

--- seeders/20240913194727-user.js
+++ seeders/20240913194727-user.js
@@ -1,38 +1,38 @@
-'use strict'
-const { v4: uuidv4 } = require('uuid')
-const { encrypt } = require('../utils/handlePassword')
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+const { encrypt } = require("../utils/handlePassword");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     const roles = await queryInterface.sequelize.query(
-      'SELECT id, name FROM `Rols`;',
+      "SELECT id, name FROM `Rols`;",
       { type: queryInterface.sequelize.QueryTypes.SELECT }
-    )
+    );
 
     const zones = await queryInterface.sequelize.query(
-      'SELECT id, name FROM `Zones`;',
+      "SELECT id, name FROM `Zones`;",
       { type: queryInterface.sequelize.QueryTypes.SELECT }
-    )
+    );
 
-    const rol = {}
-    roles.forEach(r => {
-      if (r.name === 'Superadministrador') rol.superAdmin = r.id
-    })
+    const rol = {};
+    roles.forEach((r) => {
+      if (r.name === "Superadministrador") rol.superAdmin = r.id;
+    });
 
-    const zone = {}
-    zones.forEach(z => {
-      if (z.name === 'EJE CAFETERO') zone.eje = z.id
-    })
+    const zone = {};
+    zones.forEach((z) => {
+      if (z.name === "EJE CAFETERO") zone.eje = z.id;
+    });
 
-    const encryptedPassword = await encrypt('T@zmania12345678')
+    const encryptedPassword = await encrypt("T@zmania12345678");
 
-    return queryInterface.bulkInsert('Users', [
+    return queryInterface.bulkInsert("Users", [
       {
         id: uuidv4(),
-        name: 'Crisa',
-        lastName: 'cruz',
-        email: 'cricacrugo97@gmail.com',
+        name: "Jhonatan",
+        lastName: "Velandia",
+        email: "velandiajhonata07@gmail.com",
         cedula: 1111111111,
         phone: 3333333333,
         password: encryptedPassword,
@@ -41,12 +41,12 @@ module.exports = {
         active: true,
         failedAttempts: 0,
         createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ])
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('Users', null, {})
-  }
-}
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", null, {});
+  },
+};
--- services/authService.js
+++ services/authService.js
@@ -1,175 +1,202 @@
-const { User, Rol } = require('../models')
-const { encrypt, compare } = require('../utils/handlePassword')
-const { tokenSign, tokenResetPassword, verifyTokenResetPassword } = require('../utils/handleJwt')
-const { handleHttpError } = require('../utils/handleError')
-const sendMail = require('../email/email')
-const ejs = require('ejs')
-const path = require('path')
+const { User, Rol } = require("../models");
+const { encrypt, compare } = require("../utils/handlePassword");
+const {
+  tokenSign,
+  tokenResetPassword,
+  verifyTokenResetPassword,
+} = require("../utils/handleJwt");
+const { handleHttpError } = require("../utils/handleError");
+const sendMail = require("../email/email");
+const ejs = require("ejs");
+const path = require("path");
 
 const registerUser = async function (body) {
   try {
     if (body.password !== body.verifyPassword) {
-      throw new Error('Las contraseñas no coinciden')
+      throw new Error("Las contraseñas no coinciden");
     }
 
     const rol = await Rol.findOne({
-      where: { name: 'Comercial' }
-    })
+      where: { name: "Comercial" },
+    });
 
     if (!rol) {
-      throw new Error('El rol proporcionado no existe')
+      throw new Error("El rol proporcionado no existe");
     }
 
-    const encryptedPassword = await encrypt(body.password)
+    const encryptedPassword = await encrypt(body.password);
 
     const newUser = await User.create({
-      fullName: body.fullName,
+      name: body.name, // Cambiar fullName por name
+      lastName: body.lastName, // Agregar lastName
       email: body.email,
       password: encryptedPassword,
       idRol: rol.id,
-      active: true
-    })
+      active: true,
+    });
 
-    const token = await tokenSign(newUser)
-    await newUser.update({ jwt: token })
-    const userWithoutPassword = newUser.toJSON()
-    delete userWithoutPassword.password
-    delete userWithoutPassword.jwt
-    delete userWithoutPassword.active
-    delete userWithoutPassword.failedAttempts
+    const token = await tokenSign(newUser);
+    await newUser.update({ jwt: token });
+    const userWithoutPassword = newUser.toJSON();
+    delete userWithoutPassword.password;
+    delete userWithoutPassword.jwt;
+    delete userWithoutPassword.active;
+    delete userWithoutPassword.failedAttempts;
 
-    return { userWithoutPassword, token }
+    return { userWithoutPassword, token };
   } catch (error) {
-    throw new Error(`Error al crear el user: ${error.message}`)
+    throw new Error(`Error al crear el user: ${error.message}`);
   }
-}
+};
 
 const loginUser = async function (res, body) {
   try {
-    const { password, email } = body
+    const { password, email } = body;
 
     const user = await User.findOne({
       where: { email },
-      attributes: ['id', 'password', 'fullName', 'idRol', 'email', 'jwt', 'active', 'failedAttempts']
-    })
+      attributes: [
+        "id",
+        "password",
+        "name",
+        "lastName",
+        "idRol",
+        "email",
+        "jwt",
+        "active",
+        "failedAttempts",
+      ],
+    });
 
-    if (!user) return handleHttpError(res, 'Correo o contraseña incorrectos', 401)
+    if (!user)
+      return handleHttpError(res, "Correo o contraseña incorrectos", 401);
 
-    if (!user.active) return handleHttpError(res, 'Usuario desactivado. Contacte al administrador.', 403)
+    if (!user.active)
+      return handleHttpError(
+        res,
+        "Usuario desactivado. Contacte al administrador.",
+        403
+      );
 
-    const check = await compare(password, user.password)
+    const check = await compare(password, user.password);
     if (!check) {
-      user.failedAttempts += 1
-      await user.save()
+      user.failedAttempts += 1;
+      await user.save();
 
       if (user.failedAttempts >= 5) {
-        user.active = false
-        await user.save()
-        return handleHttpError(res, 'Usuario desactivado. Contacte al administrador.', 403)
+        user.active = false;
+        await user.save();
+        return handleHttpError(
+          res,
+          "Usuario desactivado. Contacte al administrador.",
+          403
+        );
       }
 
-      return handleHttpError(res, 'Correo o contraseña incorrectos', 401)
+      return handleHttpError(res, "Correo o contraseña incorrectos", 401);
     }
 
-    user.failedAttempts = 0
-    await user.save()
+    user.failedAttempts = 0;
+    await user.save();
 
-    const token = await tokenSign(user)
-    await user.update({ jwt: token })
+    const token = await tokenSign(user);
+    await user.update({ jwt: token });
 
-    const userWithoutSensitiveData = user.toJSON()
-    delete userWithoutSensitiveData.password
-    delete userWithoutSensitiveData.jwt
-    delete userWithoutSensitiveData.failedAttempts
+    const userWithoutSensitiveData = user.toJSON();
+    delete userWithoutSensitiveData.password;
+    delete userWithoutSensitiveData.jwt;
+    delete userWithoutSensitiveData.failedAttempts;
 
     return {
       token,
-      user: userWithoutSensitiveData
-    }
+      user: userWithoutSensitiveData,
+    };
   } catch (error) {
-    throw new Error(`Error al iniciar sesión: ${error.message}`)
+    throw new Error(`Error al iniciar sesión: ${error.message}`);
   }
-}
+};
 
 const logoutUser = async function (userId) {
   try {
-    const user = await User.findByPk(userId.id)
+    const user = await User.findByPk(userId.id);
 
     if (!user) {
-      throw new Error('Usuario no encontrado')
+      throw new Error("Usuario no encontrado");
     }
 
-    await user.update({ jwt: null })
+    await user.update({ jwt: null });
 
-    return { message: 'Sesión cerrada correctamente' }
+    return { message: "Sesión cerrada correctamente" };
   } catch (error) {
-    throw new Error(`Error al cerrar sesión: ${error.message}`)
+    throw new Error(`Error al cerrar sesión: ${error.message}`);
   }
-}
+};
 
 const forgotPassword = async function (email) {
   try {
-    const url = process.env.PUBLIC_URL
+    const url = process.env.PUBLIC_URL;
     const user = await User.findOne({
-      where: { email: email.email }
-    })
+      where: { email: email.email },
+    });
 
     if (!user) {
-      throw new Error('El email proporcionado no existe')
+      throw new Error("El email proporcionado no existe");
     }
 
-    const token = await tokenResetPassword(user)
-    const resetUrl = `${url}/cambio-contrasena/${token}`
+    const token = await tokenResetPassword(user);
+    const resetUrl = `${url}/cambio-contrasena/${token}`;
 
     const htmlTemplate = await ejs.renderFile(
-      path.join(__dirname, '../email/templates/password.ejs'),
-      { fullName: user.fullName, resetUrl }
-    )
+      path.join(__dirname, "../email/templates/password.ejs"),
+      { name: user.name, lastName: user.lastName, resetUrl }
+    );
 
-    await sendMail(user.email, 'Restablecimiento de Contraseña', htmlTemplate)
+    await sendMail(user.email, "Restablecimiento de Contraseña", htmlTemplate);
 
-    return { message: 'Correo de restablecimiento de contraseña enviado correctamente' }
+    return {
+      message: "Correo de restablecimiento de contraseña enviado correctamente",
+    };
   } catch (error) {
-    console.log(error)
-    throw new Error('Error al enviar el email')
+    console.log(error);
+    throw new Error("Error al enviar el email");
   }
-}
+};
 
 const resetPassword = async function (token, body) {
   try {
-    const verifyToken = await verifyTokenResetPassword(token)
+    const verifyToken = await verifyTokenResetPassword(token);
     if (!verifyToken) {
-      throw new Error('El token no es válido')
+      throw new Error("El token no es válido");
     }
 
     const user = await User.findOne({
-      where: { email: verifyToken.email }
-    })
+      where: { email: verifyToken.email },
+    });
     if (!user) {
-      throw new Error('El email proporcionado no existe')
+      throw new Error("El email proporcionado no existe");
     }
 
     if (body.password !== body.verifyPassword) {
-      throw new Error('Las contraseñas no coinciden')
+      throw new Error("Las contraseñas no coinciden");
     }
 
-    const encryptedPassword = await encrypt(body.password)
+    const encryptedPassword = await encrypt(body.password);
 
     await User.update(
       { password: encryptedPassword },
       { where: { email: verifyToken.email } }
-    )
+    );
 
-    return { message: 'Cambio de contraseña exitoso' }
+    return { message: "Cambio de contraseña exitoso" };
   } catch (error) {
-    throw new Error(`Error al cambiar la contraseña: ${error.message}`)
+    throw new Error(`Error al cambiar la contraseña: ${error.message}`);
   }
-}
+};
 
 module.exports = {
   registerUser,
   loginUser,
   logoutUser,
   forgotPassword,
-  resetPassword
-}
+  resetPassword,
+};
