95736a5664632adffd5add0990359b9e1057c06f
LuisPJ <luisandres-perez@hotmail.es>
2024-12-10 14:01:28 -0500

    Se añadio cotizaciones y ventas

--- seeders/20240905211633-company.js
+++ seeders/20240905211633-company.js
@@ -1,16 +1,32 @@
 'use strict';
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Companies', [{
-      id: 1,
-      company_name: 'kuva',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    const companies = [
+      {
+        id: uuidv4(),
+        name: 'Kuva',
+        legalName: 'Kuva',
+        email: 'contact@kuva.com',
+        phone: '+571234567890',
+        address: 'Calle 123 #45-67, Bogotá, Colombia',
+        website: 'https://www.kuva.com',
+        industry: 'Technology',
+        clientId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ];
+
+    await queryInterface.bulkInsert('Companies', companies, {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Companies', null, { where: { id: 1 } });
-  }
+    await queryInterface.bulkDelete('Companies', null, {
+      where: {
+        email: 'contact@kuva.com',
+      },
+    });
+  },
 };
