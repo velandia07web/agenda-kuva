feef4b101cac40c93293fe473256835def8ecc8f
LuisPJ <luisandres-perez@hotmail.es>
2025-01-06 21:21:20 -0500

    Fix bugs

--- seeders/20240914004152-product.js
+++ seeders/20240914004152-product.js
@@ -478,7 +478,7 @@ module.exports = {
       imagen: product.imagen,
       description: product.description,
       idZone: product.idZone,
-      count: 10000,
+      count: 50,
       state: "ACTIVO",
       createdAt: product.createdAt,
       updatedAt: product.updatedAt
